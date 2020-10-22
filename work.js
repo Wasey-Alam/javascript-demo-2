3c12abfcec0bbf27cf00de5e82727f32f4dee367
Wasey-Alam <wasey.alam11@gmail.com>
2020-10-22 19:06:04 +0500

    no commnet

---
+++ work.js
@@ -0,0 +1,493 @@
+
+// var name=prompt("Enter user name")
+// console.log (name)
+// alert ("Hi " +name)
+
+
+// var table=prompt("table")
+// console.log (table)
+ 
+// if (table){
+// console.log (table+ " x " +  "1" + " = "+  table*1)
+// console.log (table+ " x " +  "2" + " = "+  table*2)
+// console.log (table+ " x " +  "3" + " = "+  table*3)
+// console.log (table+ " x " +  "4" + " = "+  table*4)
+// console.log (table+ " x " +  "5" + " = "+  table*5)
+// console.log (table+ " x " +  "6" + " = "+  table*6)
+// console.log (table+ " x " +  "7" + " = "+  table*7)
+// console.log (table+ " x " +  "8" + " = "+  table*8)
+// console.log (table+ " x " +  "9" + " = "+  table*9)
+// console.log (table+ " x " +  "10" + " = "+ table*10)
+// }
+
+
+// else{
+
+//     var table_2=5
+//     console.log (table_2)
+
+// console.log (table_2+ " x " +  "1" + " = "+  table_2*1)
+// console.log (table_2+ " x " +  "2" + " = "+  table_2*2)
+// console.log (table_2+ " x " +  "3" + " = "+  table_2*3)
+// console.log (table_2+ " x " +  "4" + " = "+  table_2*4)
+// console.log (table_2+ " x " +  "5" + " = "+  table_2*5)
+// console.log (table_2+ " x " +  "6" + " = "+  table_2*6)
+// console.log (table_2+ " x " +  "7" + " = "+  table_2*7)
+// console.log (table_2+ " x " +  "8" + " = "+  table_2*8)
+// console.log (table_2+ " x " +  "9" + " = "+  table_2*9)
+// console.log (table_2+ " x " +  "10" + " = "+ table_2*10)
+    
+// }
+
+
+
+
+
+// var cityName=prompt("Enter the city name")
+// console.log (cityName)
+// if (cityName==="karachi"){
+//     alert("welcome to the city of light")
+// }
+// else{
+//     alert("error")
+// }
+
+
+
+// start
+
+// var gender=prompt("please enter the gender")
+// console.log (gender)
+
+// if (gender==="man"){
+//     alert("good morning sir")
+// }
+
+// else if (gender==="women"){
+//      alert("good morning mam")
+// }
+
+// else{
+//     alert("please enter man or women")
+// }
+
+
+// end
+
+
+
+
+
+
+
+
+
+
+
+// var color=prompt("Enter the color")
+// console.log(color)
+// if (color==="red"){
+//     alert("vehicle must be stop")
+
+// }
+
+// else if(color==="yellow"){
+//     alert("start")
+    
+// }
+
+// else if(color==="green"){
+//     alert("run")
+// }
+// else {
+//     alert("error")
+// }
+
+
+
+
+
+// var urdu=+prompt("marks in urdu")
+// console.log(urdu)
+
+// var math=+prompt("marks in math")
+// console.log(math)
+
+// var english=+prompt("marks in english")
+// console.log(english)
+
+
+// var total_marks=300
+
+// var obtain=english+urdu+math
+//   console.log("otain***"+ obtain)
+
+// var percentage=obtain / total_marks * 100
+// console.log("percentage***"+ percentage)
+
+// if(percentage>=80){
+//     console.log("A1")
+//     console.log("excellent")
+// }
+
+// else if (percentage>=70){
+//       console.log("A")
+//       console.log("good")
+// }
+
+// else if (percentage>=60){
+//     console.log("B")
+//     console.log("bad")
+
+// }
+// else if (percentage>=50){
+//     console.log("c")
+//     console.log("nhi ho paye ga")
+// }
+
+
+
+// start
+
+// var remainingFuel=+prompt("remaining fuel in the car")
+// console.log(remainingFuel + "L")
+
+// if (remainingFuel<=1){
+//     alert("please refuel")
+// }
+// else{
+//     alert("no worry")
+// }
+
+// end
+
+
+// start
+
+// var name_of_item1=prompt("name of item u would selected")
+// console.log (name_of_item1)
+
+// var name_of_item2=prompt("name of item 2 u would selected")
+// console.log (name_of_item2)
+
+// var price_of_item1=prompt("price of item 1")
+// console.log (price_of_item1)
+
+// var price_of_item2=prompt("price of item 2")
+// console.log (price_of_item2)
+
+// var ordered_Q1=prompt("quantity of item1")
+// console.log (ordered_Q1)
+
+// var ordered_Q2=prompt("quantity of item2")
+// console.log (ordered_Q2)
+
+// var shiping=150
+
+// var total_of_all=( price_of_item1 * ordered_Q1 + price_of_item2 * ordered_Q2 + shiping +"total cost of your ordered")
+// console.log (total_of_all)
+
+// var total=total_of_all/100*10
+// console.log (total)
+
+
+// if (total<=2000){
+
+//     alert("order sucess")
+     
+// }
+// else{
+//     alert("no")
+// }
+
+
+
+// start
+
+// var num1=+prompt("enter the number")
+// console.log (num1)
+
+// if (num1===6){
+//     alert("bingo")
+// }
+
+// else if(num1===5){
+//         alert("to close")
+// }
+
+// else if(num1===7){
+//     alert("to close")
+// }
+// else{
+//     alert("wrong")
+// }
+
+// end
+
+// start
+
+// var team1=+prompt("enter the score of team1")
+// console.log (team1)
+
+// var team2=+prompt("enter the score of team2")
+// console.log (team2)
+
+// if (team1>=team2){
+//     alert("team1 win the match")
+
+// }
+// else if(team2>=team1){
+//     alert("team2 win the match")
+// }
+// else if(team1===team2){
+//     alert("tie")
+// }
+// else{
+//     alert("wrong")
+// }
+
+
+
+// var alphabet=prompt("Enter the word")
+//   console.log(alphabet)
+
+
+// if (typeof alphabet==="string"){
+//     alert("this is a string")
+    
+// }
+
+// else if(1==10){
+//     alert("this is number")
+// }
+
+
+// var num =prompt("enetr no")
+
+//    console.log(num);
+
+//    if(num % 2 == 0) {
+//       alert('Number is even!');
+
+//    }
+   
+//    else {
+//     alert('Number is odd!');
+//    }
+
+// var temp=+prompt("enter the temperature")
+// console.log(temp)
+
+// if(temp>=40){
+//     alert("It is too hot outside")
+// }
+
+// else if(temp>=30){
+//     alert("“The Weather today is Normal.”")
+// }
+
+// else if(temp>=20){
+//     alert("“Today’s Weather is cool.”")
+// }
+
+
+// else if(temp>=10){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+
+// else{
+//     alert("wrong")
+// }
+
+
+// var firstNumber=+prompt("enter first number")
+// var secondNumber=+prompt("enter second number")
+// var totalin=prompt("enter the sumbol")
+
+// console.log (firstNumber)
+// console.log (secondNumber)
+// console.log (totalin)
+
+// var totaladd=firstNumber+secondNumber
+
+// var totalsub=firstNumber-secondNumber
+
+// var totalmuliti=firstNumber*secondNumber
+
+// var totaldivide=firstNumber/secondNumber
+
+// if (totaladd==="add"){
+//     alert("add")
+// }
+
+// else if(totalsub==="sub"){
+//     alert("sub")
+// }
+
+// var dayinput=prompt("Enter the day")
+// console.log (dayinput)
+
+// if (dayinput==="monday"){
+//     alert("It’s a week day")
+// }
+
+// else if (dayinput==="tuesady"){
+//     alert("It’s a week day")
+// }
+
+
+// else if (dayinput==="wednesday"){
+//     alert("It’s a week day")
+// }
+
+
+// else if (dayinput==="thursday"){
+//     alert("It’s a week day")
+// }
+
+
+// else if (dayinput==="firday"){
+//     alert("It’s a week day")
+// }
+
+
+// else if (dayinput==="saturday"){
+//     alert("It’s weekend.")
+// }
+
+// else if (dayinput==="sunday"){
+//     alert("Yay! It’s a holiday")
+// }
+
+// else{
+//     alert("spalling mistake")
+// }
+
+// var lang1=prompt("enter the language")
+// console.log (lang1)
+
+// if(lang1==="en"){
+//     alert("hello world")
+// }
+
+// else if(lang1==="es"){
+//     alert("Saluton mondo")
+// }
+
+// else if(lang1==="de"){
+//     alert("Hallo Wereld")
+// }
+//  else{
+//      alert("chose {en,es,de}")
+//  }
+
+
+// var noun=prompt("enter noun")
+// var numberNoun=prompt("enter the number")
+
+// console.log (noun)
+// console.log (numberNoun)
+
+// alert (noun+numberNoun)
+
+
+
+// IF...ELSE & ELSE IF STATEMENTS, TESTING SET OF CONDITIONS & SWITCH STATEMENTS | JAVASCRIPT
+
+
+// var number_input=+prompt("enter the number")
+// console.log (number_input)
+
+// var total_input=(number_input/3)
+// console.log (total_input)
+
+// if (total_input){
+//     alert("yes divide")
+// }
+// else{
+//     alert("no")
+// }
+
+
+// var ageOfUser=+prompt("enter the user age")
+// console.log (ageOfUser)
+
+// if (ageOfUser<18){
+//     alert("old enough")
+// }
+// else{
+//     alert("too young")
+// }
+
+
+// var nameOfuser=prompt("enter the user name")
+// console.log (nameOfuser)
+
+// if(nameOfuser==="wasey"){
+//     alert("hello world")
+// }
+// else{
+//     alert ("no thhing")
+// }
+
+
+// var passwordOfMe="waseyalam"
+// var paswordInput=prompt("enter the password")
+// console.log (paswordInput)
+
+// if(passwordOfMe===paswordInput){
+//     alert("correct")
+// }
+
+// else if(passwordOfMe===paswordInput){
+//     alert("wrong")
+// }
+// else {
+//     alert("retype password")
+// }
+
+
+// var greeting;
+
+// var hour = 13;
+
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+
+// else{greeting = "Good evening";
+
+// }
+
+
+
+
+// var num = prompt("enter the number")
+
+// if(num >> 31 === -1){
+//     alert('Negative number')
+// }
+// else{
+//     alert('Positive number')
+// }
+
+
+
+// var hour=+prompt("enter the hour")
+// console.log (hour)
+
+// if(hour=6<9){
+//    alert("Breakfast is served")
+// }
+
+// else if(hour=11<4){
+//     alert("time for lunch")
+// }
+
+// else if(hour=5<8){
+//     alert("time for dinner")
+// }
+
+// else if(hour=24){
+//     alert("Sorry, you'll have to wait, or go get a snack.")
+// }
